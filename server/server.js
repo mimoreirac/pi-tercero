4c26b104c3972821851035a17811adc8a9d34426
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-07-28 20:57:06 -0500

    Revert "Crud viajes"

--- server/server.js
+++ server/server.js
@@ -1,14 +1,1 @@
-import express from "express";
-import pool from "./db.js";
-import usuarioRoutes from "./routes/usuarioRoutes.js";
-import viajeRoutes from "./routes/viajeRoutes.js";
-
-const port = process.env.PORT;
-const app = express();
-
-app.use(express.json());
-
-app.use(usuarioRoutes);
-app.use(viajeRoutes);
-
-export default app;
+console.log("Hola mundo");
--- vitest.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    setupFiles: ['./server/tests/setup.js'],
-  },
-});
