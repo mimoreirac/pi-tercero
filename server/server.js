0ec9eac597b023b2de41c5c32ab82adae1dcd61e
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-07-28 20:51:25 -0500

    Agrega CRUD de viajes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import pool from "./db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import viajeRoutes from "./routes/viajeRoutes.js";
 
 const port = process.env.PORT;
 const app = express();
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.use(usuarioRoutes);
+app.use(viajeRoutes);
 
 export default app;
