ab7d516fe2d0666e6ce6dde6f7aaa48e961c5afe
Caleb San Lucas <cssan@puce.edu.ec>
2025-08-11 19:38:04 -0500

    Agregar CRUD viajes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import pool from "./db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 import viajeRoutes from "./routes/viajeRoutes.js";
+import incidentesRoutes from "./routes/incidentesRoutes.js";
 
 const port = process.env.PORT;
 const app = express();
@@ -10,5 +11,6 @@ app.use(express.json());
 
 app.use(usuarioRoutes);
 app.use(viajeRoutes);
+app.use(incidentesRoutes);
 
 export default app;
